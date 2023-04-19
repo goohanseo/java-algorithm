df3fe11d6f85decf041b5d73e6f448d6da034be1
PKNU <gooruem2@naver.com>
2023-04-19 11:05:27 +0900

    asdf

---
+++ src/week3/nqueens.java
@@ -0,0 +1,17 @@
+//public class nqueens {
+//    int cols[N+1]
+//    int sum = 0;
+//    int queens(int level){
+//        if (!promising(level))
+//            return 0;
+//        else if (level ==N)
+//            return 1;
+//        for (int i =1; i<N; i++){
+//            cols[level+1] = i;
+//            if(queens(level+1))
+//                sum += queens(level+1)
+//                return sum;
+//        }
+//        return false;
+//    }
+//}
